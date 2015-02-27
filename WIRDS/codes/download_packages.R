cf1364d663aabef23ca1229c6c97516bf5c72b48
Maciej Beręsewicz <MaciejBeresewicz@pc12.home>
2015-02-27 10:05:10 +0100

    download packages

---
+++ WIRDS/codes/download_packages.R
@@ -0,0 +1,8 @@
+### instalacja pakietow
+
+install.packages(c('devtools','dplyr','tidyr',
+                   'ggplot2','sjPlot','car',
+                   'scales','RColorBrewer','XLConnect',
+                   'knitr','rmarkdown','xtable',
+                   'shiny','miniCRAN','Cairo','stringi',
+                   'XML'), dependencies =T)
