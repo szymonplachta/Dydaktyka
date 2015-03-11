3a9013ac683a7fceaca7427f501da9305b52fde2
szymonplachta <simson92@gmail.com>
2015-03-11 11:04:56 +0100

    Naniesione poprawki

--- WIRDS/homework/praca_domowa_1_Płachta.R
+++ WIRDS/homework/praca_domowa_1_Płachta.R
@@ -1,6 +1,7 @@
 library(XLConnect)
 library(dplyr)
 library(ggplot2)
+library(scales)
 fName <- "C:/Users/Szymon/Desktop/gospodarstwa.xls"
 wb <- loadWorkbook(fName)
 gos <- readWorksheet(wb, sheet = "gospodarstwa")
@@ -11,12 +12,18 @@ gos$woj <- factor(gos$woj,
                              "ma³opolskie", "mazowieckie", "opolskie", "podkarpackie", "podlaskie",
                              "pomorskie", "œl¹skie", "œwiêtokrzyskie", "warmiñsko-mazurskie",
                              "wielkopolskie", "zachodniopomorskie"))
+gos$klm <- factor(gos$klm, 
+                  levels = 6:1, 
+                  labels = c("wieœ", "miasto<20","[20,100)","[100,200)","[200,500)",">=500"), ordered=T)
+         
 gos %>% 
   count(klm,woj) %>%
   group_by(woj) %>%
   mutate(proc = n/sum(n)) %>%
   ggplot(data = ., aes(x = " ", y = proc, group = klm, fill = klm)) +
   xlab("województwo") +
+  scale_y_continuous("odsetek respondentów", labels =percent) +
   geom_bar(stat = 'identity', col='black') +
   facet_wrap(~woj, ncol=1) + 
-  coord_flip()
+  coord_flip() +
+  ggtitle("Udzia³ respondentów wed³ug klasy miejscowoœci")
